bd61f9fa3590eeacad2bf64acd66257d3961558f
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-05-10 17:41:35 +0300

    SCRAB-83 added buttons to view

--- src/main/java/com/example/scrabble/HelloController.java
+++ src/main/java/com/example/scrabble/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Lama Omer Kaha Kaved?!");
     }
 }
