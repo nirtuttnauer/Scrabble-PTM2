72e0484641aa5da7dcd14d2ebc6be58bd923cf5b
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-05-14 14:55:32 +0300

    SCRAB-104 Created SceneController and updated views

--- src/main/java/com/example/scrabble/BookScrabbleController.java
+++ src/main/java/com/example/scrabble/BookScrabbleController.java
@@ -1,13 +1,18 @@
 package com.example.scrabble;
 
+import com.example.scrabble.model.GuestModel;
+import com.example.scrabble.model.HostModel;
 import com.example.scrabble.model.StartModel;
 import com.example.scrabble.model.iModel;
+import com.example.scrabble.vm.GuestVM;
+import com.example.scrabble.vm.HostVM;
 import com.example.scrabble.vm.StartVM;
 import com.example.scrabble.vm.iVM;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import com.example.scrabble.model.HostModel;
-import com.example.scrabble.vm.HostVM;
+
+import java.io.IOException;
 
 public class BookScrabbleController {
     @FXML
@@ -15,23 +20,43 @@ public class BookScrabbleController {
 
     private iVM viewModel;
 
+    private SceneController sc = new SceneController();
+
     public void initialize() {
         iModel model = new StartModel();
         viewModel = new StartVM(model);
-        MainTitle.textProperty().bind(viewModel.welcomeMessageProperty());
+    }
+
+    @FXML
+    protected void onJoinButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "join-view.fxml");
+        viewModel = new GuestVM(new GuestModel());
+    }
+
+    @FXML
+    public void onSettingButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "settings-view.fxml");
+    }
+
+    @FXML
+    public void onHostButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "host-view.fxml");
+        viewModel = new HostVM(new HostModel());
     }
     @FXML
-    protected void onJoinButtonClick() {
-        viewModel.onJoinButtonClick();
+    public void onLobbyButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "lobby-view.fxml");
     }
-    @FXML
-    public void onSettingButtonClick() {
-        viewModel.onSettingButtonClick();
+
+    public void onGameButtonClick(ActionEvent event) throws IOException {
+          sc.setScene(event, "game-view.fxml");
     }
-    @FXML
-    public void onHostButtonClick() {
-        iModel model = new HostModel();
-        viewModel = new HostVM((HostModel) model);
-        MainTitle.textProperty().bind(viewModel.welcomeMessageProperty());
+
+    public void onMenuButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "menu-view.fxml");
+    }
+
+    public void onFinishButtonClick(ActionEvent event) throws IOException {
+        sc.setScene(event, "leaderboard-view.fxml");
     }
 }
