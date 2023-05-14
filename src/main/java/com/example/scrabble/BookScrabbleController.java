29b3e17aeace10579922ec69debc149eac40f974
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-05-14 13:18:16 +0300

    SCRAB-101 updated file system

--- src/main/java/com/example/scrabble/BookScrabbleController.java
+++ src/main/java/com/example/scrabble/BookScrabbleController.java
@@ -4,7 +4,6 @@ import com.example.scrabble.model.StartModel;
 import com.example.scrabble.model.iModel;
 import com.example.scrabble.vm.StartVM;
 import com.example.scrabble.vm.iVM;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import com.example.scrabble.model.HostModel;
@@ -12,27 +11,27 @@ import com.example.scrabble.vm.HostVM;
 
 public class BookScrabbleController {
     @FXML
-    private Label welcomeText;
+    private Label MainTitle;
 
     private iVM viewModel;
 
     public void initialize() {
         iModel model = new StartModel();
         viewModel = new StartVM(model);
-        welcomeText.textProperty().bind(viewModel.welcomeMessageProperty());
+        MainTitle.textProperty().bind(viewModel.welcomeMessageProperty());
     }
-
-    @FXML
-    protected void onHelloButtonClick() {
-        viewModel.onHelloButtonClick();
-    }
-
     @FXML
     protected void onJoinButtonClick() {
         viewModel.onJoinButtonClick();
     }
-
+    @FXML
     public void onSettingButtonClick() {
         viewModel.onSettingButtonClick();
+    }
+    @FXML
+    public void onHostButtonClick() {
+        iModel model = new HostModel();
+        viewModel = new HostVM((HostModel) model);
+        MainTitle.textProperty().bind(viewModel.welcomeMessageProperty());
     }
 }
