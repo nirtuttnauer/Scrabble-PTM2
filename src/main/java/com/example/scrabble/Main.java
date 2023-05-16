68702ded456c32debd09b295955f1808c066a18f
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-05-16 13:21:37 +0300

    SCRAB-105 stable I NEED SLEEP 3

--- src/main/java/com/example/scrabble/Main.java
+++ src/main/java/com/example/scrabble/Main.java
@@ -8,30 +8,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-    private static Stage stage;
+    private static Stage window;
     static SceneFactory sceneFactory;
 
 
 
 
-    public static Stage getStage() {
-        return stage;
+    public static Stage getWindow() {
+        return window;
     }
 
-    private static void setStage(Stage stage) throws IOException {
-        Main.stage = stage;
+    private static void setWindow(Stage window) throws IOException {
+        Main.window = window;
         sceneFactory = new SceneFactory();
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        setStage(primaryStage);
+        setWindow(primaryStage);
         setScene("WelcomeView");
     }
 
     public static void setScene(String path) throws IOException {
-        getStage().setScene(sceneFactory.getSceneMap().get(path));
-        getStage().show();
+        getWindow().setScene(sceneFactory.getSceneMap().get(path));
+        getWindow().show();
     }
 
 
