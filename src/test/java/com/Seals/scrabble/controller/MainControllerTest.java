8619e1106c8628c6b76f7974db424c4dbebbc735
an1604 <nataf12386@gmail.com>
2023-05-20 16:36:41 +0300

    updated tests

---
+++ src/test/java/com/Seals/scrabble/controller/MainControllerTest.java
@@ -0,0 +1,20 @@
+package com.Seals.scrabble.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void onExitButtonClick() {
+    }
+
+    @Test
+    void onSwitchButtonClick() {
+    }
+}
---
+++ src/test/java/com/Seals/scrabble/controller/SettingsControllerTest.java
@@ -0,0 +1,28 @@
+package com.Seals.scrabble.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SettingsControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void onSwitchButtonClick() {
+    }
+
+    @Test
+    void onChangeNameButtonClick() {
+    }
+
+    @Test
+    void onRunTestsButtonClick() {
+    }
+
+    @Test
+    void onMenuButtonClick() {
+    }
+}
