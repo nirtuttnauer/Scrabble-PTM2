22bf9f71dffd47fd8cb61bd4232aab5fd3daffd2
OmerYaakov <omeryaakov2@gmail.com>
2023-06-08 12:26:56 +0300

    update ViewModel

--- src/main/java/com/Seals/scrabble/viewmodel/ViewModel.java
+++ src/main/java/com/Seals/scrabble/viewmodel/ViewModel.java
@@ -4,7 +4,6 @@ import com.Seals.scrabble.Settings;
 import com.Seals.scrabble.facade.ModelFacade;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -15,22 +14,28 @@ public class ViewModel extends Observable implements Observer, iViewModel {
 
     private final ModelFacade modelFacade;
 
-     private static StringProperty hand;
 
-    public static StringProperty getHand() {
-        return hand;
+    private static StringProperty handToView;
+     private String newHand;
+     private String handFromModel;
+    private final int[] values = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+    public static StringProperty getHandToView() {
+        return handToView;
     }
 
     public StringProperty handProperty() {
-        return hand;
+        return handToView;
     }
 
     public ViewModel() {
-        this.hand= new SimpleStringProperty();
-        hand.set("ABBBCAS");
+        handFromModel = new String();
+        newHand = new String();
+        handToView = new SimpleStringProperty();
         sharedInstance = this;
         modelFacade = new ModelFacade();
         this.nickname = new SimpleStringProperty();
+//        check();
     }
 
     public static iViewModel getSharedInstance() {
@@ -62,11 +67,16 @@ public class ViewModel extends Observable implements Observer, iViewModel {
 
     @Override
     public void update(Observable o, Object arg) {
-//         if (o == modelFacade) {
-//             nickname.set(model.getNickname());
+//        if (o instanceof ModelFacade) {
+//            if (arg instanceof String) {
+//                String check = arg.toString();
+//                if(check.charAt(0) == 'U' && check.charAt(1) == 'A' && check.charAt(2) == ',')
+//                    handFromModel = arg.toString();
+//                    setLetterValue(handFromModel);
+//            }
 //         }
+//            nickname.set(model.getNickname());
     }
-
     // Additional methods and functionality specific to ViewModel
 
 //     public void startServer() {
@@ -84,4 +94,30 @@ public class ViewModel extends Observable implements Observer, iViewModel {
     public void joinGame() {
         modelFacade.joinGame(Settings.getServerAddress(), Settings.getHostServerPort());
     }
+    public void setLetterValue(String tiles){
+        for (int i = 3; i < handFromModel.length(); i++){
+            int value = values[tiles.charAt(i) - 'A'];
+            newHand += handFromModel.charAt(i) + "," + value +",";
+        }
+        newHand = newHand.substring(0, newHand.length()-1);
+        setHandToView();
+    }
+
+    public void setHandToView(){
+        handToView.set(newHand);
+    }
+
+    public static StringProperty handToViewProperty() {
+        return handToView;
+    }
+
+    public static void setHandToView(String handToView) {
+        ViewModel.handToView.set(handToView);
+    }
+
+//    public void check(){
+//        handFromModel = "UA,AQBRTC";
+//        setLetterValue(handFromModel);
+//        System.out.println(handToView.get());
+//    }
 }
